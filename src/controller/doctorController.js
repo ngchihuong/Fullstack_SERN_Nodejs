72e88c2bea57191733f6ee20cda56d874f78fd86
Huongzzz <125900985+ngchihuong@users.noreply.github.com>
2024-07-15 15:41:25 +0700

    67 Hoàn Thiện Chứa Năng Bổ Sung Thông Tin Chi Tiết Cho Bác Sỹ

--- src/controller/doctorController.js
+++ src/controller/doctorController.js
@@ -1,7 +1,7 @@
 import doctorService from "../service/doctorServicec"
 
 
-let getTopDoctorHome = async (req,res) => {
+let getTopDoctorHome = async (req, res) => {
     let limit = req.query.limit;
     if (!limit) {
         limit = 10;
@@ -17,7 +17,31 @@ let getTopDoctorHome = async (req,res) => {
         })
     }
 }
-
+let getAllDoctors = async (req, res) => {
+    try {
+        let doctors = await doctorService.getAllDoctors();
+        return res.status(200).json(doctors)
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({
+            errcode: -1,
+            errMessage: 'Error from the server!'
+        })
+    }
+}
+let postInforDoctor = async (req,res) => {
+    try {
+        let response = await doctorService.saveDetailInforDoctor(req.body);
+        return res.status(200).json(response)
+    } catch (error) {
+        res.status(200).json({
+            errCode: -1,
+            errMessage: "Error from server!"
+        })
+    }
+}
 module.exports = {
-    getTopDoctorHome: getTopDoctorHome
+    getTopDoctorHome: getTopDoctorHome,
+    getAllDoctors: getAllDoctors,
+    postInforDoctor: postInforDoctor
 }
